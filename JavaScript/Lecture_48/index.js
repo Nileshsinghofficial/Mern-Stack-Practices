221aa22d1643131d30649a66ec5c5a509cf5a656
nileshsinghofficial <nigamsingh888@gmail.com>
2025-08-15 18:04:38 +0530

    complete 48

--- JavaScript/Lecture_48/index.js
+++ JavaScript/Lecture_48/index.js
@@ -5,6 +5,7 @@
 //         console.log(this) // full object print
 //         console.log(this.name);
 //     },
+ 
 
 //     // printName3: function(){
 //     //     console.log('Method 3')
@@ -32,12 +33,51 @@
 // console.log(prod1)
 // console.log(prod2)
 
-function Product(name, price){
-    return {name, price}
+// function Product(name, price){
+//     return {name, price}
+// }
+
+// let p1 = Product('iphone 17', 45626);
+// let p2 = Product('iphone', 45786);
+
+// console.log(p1);
+// console.log(p2);
+
+// consutroctor
+
+// function Product(name, price){
+//     this.name = name;
+//     this.price = price;
+
+
+//     phonePrice = function (){
+//         console.log(this.name)
+//     }
+// }
+
+// let p1 = new Product('Iphone-16', 56000);
+// let p2 = new Product('Iphone-17', 62000);
+
+// console.log(p1.phonePrice());
+// console.log(p2);
+
+
+
+// class
+
+class Product {
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+        this.productName = function(){
+            console.log(this.name)
+        }
+    }
+    // productName(){
+    //     console.log(this.name)
+    // }
+
 }
 
-let p1 = Product('iphone 17', 45626);
-let p2 = Product('iphone', 45786);
-
-console.log(p1);
-console.log(p2);
+let p1 = new Product('samsung', 10054);
+console.log(p1.productName())
